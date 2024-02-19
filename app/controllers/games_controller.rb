73826ba7121c115cc85004b30f8c9f9fea12c8f0
Mirela Mustic <mirela.mustic00@gmail.com>
2024-02-19 16:27:31 +0000

    added code to the score view to show the results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,22 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a.sample(10).join
+    @letters = ('A'..'Z').to_a.sample(10)
   end
 
   def score
+    @result = params[:result].split
+    @board = params[:board].split
+  end
+
+  def included?(result, board)
+    result.all? { |letter| result.count(letter) <= board.count(letter) }
+  end
+
+  def english_word(word)
+    response URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    final_word = JSON.parse(response.read)
+    final_word['found']
   end
 end
